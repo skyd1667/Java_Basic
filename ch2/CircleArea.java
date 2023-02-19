1cb142ff82e703ded703442623ed20206f99e367
skyd1667 <125776291+skyd1667@users.noreply.github.com>
2023-02-18 21:25:05 -0500

    Create CircleArea.java

---
+++ ch2/CircleArea.java
@@ -0,0 +1,10 @@
+public class CircleArea {
+    public static void main(String args[]) {
+        final double PI = 3.14;
+        double radius = 10.2;
+        double circleArea = (radius * radius * PI);
+        
+        System.out.print("radius" + radius + ",");
+        System.out.println("Area=" + circleArea);
+    }
+}
