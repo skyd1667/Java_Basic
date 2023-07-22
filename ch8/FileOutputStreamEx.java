b6186a83df8f3f34e4976eaa671b064dcf380b70
skyd1667 <125776291+skyd1667@users.noreply.github.com>
2023-07-22 17:08:26 -0400

    Create FileOutputStreamEx.java

---
+++ ch8/FileOutputStreamEx.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class FileOutputStreamEx {
+    public static void main(String[] args) {
+        byte b[] = {7, 51, 3, 4, -1, 24};
+        
+        try {
+            FileOutputStream fout = new FileOutputStream("c:\\test.out");
+            for (int i = 0; i < b.length; i++) {
+                fout.write(b[i]);
+            }
+            fout.close();
+        }
+        catch (IOException e) {}
+            System.out.println("Saved c:\\test.out");
+    }
+}
