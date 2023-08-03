d91c4bbee2c3e6e119232326a38e2b89fefd40f4
skyd1667 <125776291+skyd1667@users.noreply.github.com>
2023-08-03 17:53:10 -0400

    Create StringPrinterEx.java

---
+++ ch1/StringPrinterEx.java
@@ -0,0 +1,13 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class StringPrinterEx {
+    public static void main(String[] args) {
+        String value1 = "Hello";
+        System.out.println(value1);
+        value1 += " my name";
+        System.out.println(value1);
+        value1 += " is Tom";
+        System.out.println(value1);
+    }
+}
