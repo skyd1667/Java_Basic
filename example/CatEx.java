a27ac50a3988a15e2bc5b97f049f25a20925dbe7
Shane Kim <125776291+skyd1667@users.noreply.github.com>
2023-08-17 14:03:50 -0400

    Create CatEx.java

---
+++ example/CatEx.java
@@ -0,0 +1,14 @@
+/* 
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+ */
+public class CatEx {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( \')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
