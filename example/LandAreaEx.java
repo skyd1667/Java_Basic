372062974e067307918daa5b99e657e9cc213d19
Shane Kim <125776291+skyd1667@users.noreply.github.com>
2023-10-14 17:54:35 -0400

    Create LandAreaEx.java

---
+++ example/LandAreaEx.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LandAreaEx {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int pointCount = sc.nextInt();
+        int[] pointsX = new int[pointCount];
+        int[] pointsY = new int[pointCount];
+        for (int i = 0; i < pointCount; i++) {
+            pointsX[i] = sc.nextInt();
+            pointsY[i] = sc.nextInt();
+        }
+        int top = pointsY[0];
+        int right = pointsX[0];
+        int left = pointsX[0];
+        int bottom = pointsY[0];
+        for (int i = 0; i < pointCount; i++) {
+            if (pointsY[i] > top) top = pointsY[i];
+            if (pointsX[i] > right) right = pointsX[i];
+        }
+        for (int i = 0; i < pointCount; i++) {
+            if (pointsY[i] < bottom) bottom = pointsY[i];
+            if (pointsX[i] < left) left = pointsX[i];
+        }
+        int height = Math.abs(top - bottom);
+        int width = Math.abs(left - right);
+        System.out.println(height * width);
+    }
+}
