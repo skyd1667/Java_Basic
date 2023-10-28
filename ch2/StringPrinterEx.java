9c6d3a53707975ab852a49ce320bd2dc86228867
Shane Kim <125776291+skyd1667@users.noreply.github.com>
2023-10-28 17:57:45 -0400

    Create StringPrinterEx.java

---
+++ ch2/StringPrinterEx.java
@@ -0,0 +1,9 @@
+public class StringPrinterEx {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println(";;;;");
+        System.out.println("String " + "9980");
+        System.out.println("A");
+        System.out.println("    ");
+    }
+}
